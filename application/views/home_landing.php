6dbec9412f575b178f2b06bd6c2dfc15921604e6
Robsan <silentjanker@gmail.com>
2016-06-20 09:54:01 +0700

    disqus

--- application/views/home_landing.php
+++ application/views/home_landing.php
@@ -28,9 +28,12 @@
 					echo '<div class="blogmain">';
 						foreach ($query->result() as $row)
 						{
+								$permalink  = '/post/detail/'.$row->postId.'/'. url_title($row->postTitle);
+								$identifier = $this->uri->segment('3'); 
+								
 								echo '<div class="perpos">';
 								$img = site_url('xcrud/assets/images').'/'.$row->postImg;
-								echo anchor(site_url().'post/'.$row->postId.'/'. url_title($row->postTitle), '<h2>'.$row->postTitle.'</h2>');
+								echo anchor(site_url($permalink), '<h2>'.$row->postTitle.'</h2>');
 						        if($row->postImg != NULL)
 						        {
 						        	echo '<img src='.$img.' alt="'.$title.'" /><br/>';
@@ -39,16 +42,33 @@
 						        };
 
 						        echo '<div class="pcontent"><strong>'.$row->postDate = date('l, d F Y h:i').'</strong>';
-								echo nl2br($row->postContent).'</div></div>';
-						}
-					?>
+								echo nl2br($row->postContent).'</div></div>'; ?>
+<!-- 								Disqus commenting -->
+								<div id="disqus_thread"></div>
+								<script>
+									var disqus_config = function () {
+										this.page.url = $permalink;  // Replace PAGE_URL with your page's canonical URL variable
+										this.page.identifier = $identifier; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+									};
+									
+									(function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+										var d = document, s = d.createElement('script');
+										
+										s.src = '//janker.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+										
+										s.setAttribute('data-timestamp', +new Date());
+										(d.head || d.body).appendChild(s);
+									})();
+								</script>
+								<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+					<?php } ?>
 				</div>
 				<?php } 
 				  	elseif($this->uri->segment(1) == "page") {
 								echo '<div class="perpos">';
 						foreach ($query->result() as $row)
 						{
-								echo anchor(site_url().'page/'.$row->pageId.'/'. url_title($row->pageTitle), '<h2>'.$row->pageTitle.'</h2>');
+								echo anchor(site_url().'page/detail/'.$row->pageId.'/'. url_title($row->pageTitle), '<h2>'.$row->pageTitle.'</h2>');
 						        echo '<div class="pcontent"><strong>'.$row->pageDate = date('l, d F Y h:i').'</strong>';
 								echo nl2br($row->pageContent).'</div></div>';
 						}
@@ -82,6 +102,6 @@
 		<p><small>&mdash; Written by <a href="//jankerzone.com">Robby S. Muhammad</a></small></p>
 		</footer>
 		</div>
-		
+	<script id="dsq-count-scr" src="//janker.disqus.com/count.js" async></script>	
 	</body>
 </html>
